45f3414e31bf0aabbcea5c095dfc12c957e3cf46
Matheus <matheus.domingues126@gmail.com>
2023-04-12 13:18:31 -0300

    first commit

---
+++ app/mostrarVideos.js
@@ -0,0 +1,30 @@
+import { conexaoApi } from "./conexaoApi.js"
+
+const lista = document.querySelector('[data-lista]')
+
+export default function constroiCard(titulo, descricao, url, imagem) {
+    const video = document.createElement('li')
+    video.className = 'videos__item'
+    video.innerHTML = `
+    <iframe width="100%" height="72%" src="${url}"
+    title="${titulo}" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+    <div class="descricao-video">
+        <img src="${imagem}" alt="logo canal alura">
+        <h3>${titulo}</h3>
+        <p>${descricao}</p>
+    </div>
+    `
+    return video
+}
+
+async function listaVideos() {
+    const listaApi = await conexaoApi.listaVideos()
+    listaApi.forEach(item => lista.appendChild(
+        constroiCard(item.titulo, item.descricao, item.url, item.imagem)
+    ))
+}
+
+listaVideos()
+ 
